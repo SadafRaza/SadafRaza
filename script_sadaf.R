bdce49b4aa1c3a57550482c7966966a4195dd2ae
Sadaf Raza <82495197+SadafRaza@users.noreply.github.com>
2021-08-04 20:44:17 +0530

    Update script_sadaf.R

--- script_sadaf.R
+++ script_sadaf.R
@@ -1,9 +1,6 @@
 ##Write a script that prints your name, your email, your slack username (with @) and your biostack, twitter_handle, hamming_distance in that order.##
 
-install.packages("DescTools")
-if (!require("remotes")) install.packages("remotes")
-remotes::install_github("AndriSignorell/DescTools")
-library(DescTools)
+
 
 name <- "Sadaf Raza"
 email <- "sadafraza48@gmail.com"
@@ -11,10 +8,9 @@ biostack <- "Genomics"
 slack_username <- "@sadaf"
 twitter_handle <- "@svdvf"
 
-## Hamming distance
-hamming_distance <- StrDist(slack_username, twitter_handle, method="hamming")
 
 
-data <- data.frame(Information=c(name, email, slack_username, biostack, twitter_handle, hamming_distance))
+
+data <- data.frame(Information=c(name, email, slack_username, biostack, twitter_handle))
 
 cat(paste(data, collapse = ','))
