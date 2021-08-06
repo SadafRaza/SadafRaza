63adcd3418a7f10834805ef3d1fcb3ab888e1aa9
Sadaf Raza <82495197+SadafRaza@users.noreply.github.com>
2021-08-06 10:15:36 +0530

    Add files via upload

---
+++ script_sadaf.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("NAME: Sadaf Raza \nE-MAIL: sadafraza48@gmail.com \nSLACK USERNAME: @sadaf \nBIOSTACK: Genomics \nTwitter Handle: @svdvf")
+def hamming_distance(a,b):
+    count=0
+    for i in range(len(a)):
+        if a[i] != b[i]:
+            count +=1
+    return count
+
+print(hamming_distance('@sadaf','@svdvf'))
+
+
+# In[ ]:
+
+
+
+
